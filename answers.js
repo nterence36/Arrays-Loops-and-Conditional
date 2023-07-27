aa7b50a09d91e3121a25f1d37d4fb57583e96dcd
Terence Nkeng <nterence36@gmail.com>
2023-07-27 11:44:28 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -34,3 +34,27 @@ for (let k = 1; k <= 100; k++){
         console.log(k)
     }
 }
+
+// Wild Wild Life
+// Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2]++;
+// console.log(plantee)
+
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City"
+console.log(wolfy)
+
+// Give D'Art a second hometown by adding "Hawkins"
+dart.push("Gotham City")
+console.log(dart)
+
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy[0] = "Gameboy";
+console.log(wolfy)
